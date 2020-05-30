1d51484270eaec558d57da6d08273ff4e4b99e25
Subhashranga <bbb.ranga@gmail.com>
2020-05-30 12:33:01 +0200

    removed repeated case numbers

--- test.py
+++ test.py
@@ -31,41 +31,41 @@ class TestCalculator(unittest.TestCase):
 		solution = self.app.get('/add?A=22.222&B=98')
 		self.assertEqual(b'120.222', solution.data)
 
-	def test_add6(self):
+	def test_add5(self):
 
-		#case 6, when A is fraction p/q and B is an integer
+		#case 5, when A is fraction p/q and B is an integer
 		solution = self.app.get('/add?A=1/2&B=89')
 		self.assertEqual(b'89.5', solution.data)
 
-	def test_add7(self):
+	def test_add6(self):
 
-		#case 7, when A is an integer and B is a fraction p/q
+		#case 6, when A is an integer and B is a fraction p/q
 		solution = self.app.get('/add?A=30&B=5/2')
 		self.assertEqual(b'32.5', solution.data)
 
-	def test_add8(self):
+	def test_add7(self):
 
-		#case 8, when A input is an alphabet(non integer) and B is integer
+		#case 7, when A input is an alphabet(non integer) and B is integer
 		solution = self.app.get('/add?A=character&B=22')
 		self.assertEqual(b'22.0', solution.data)#non integer type considered as not valid , in this case which is zero
 
-	def test_add9(self):
+	def test_add8(self):
 
-		#case 9, when A input is an integer and B input is an alphabet
+		#case 8, when A input is an integer and B input is an alphabet
 		solution = self.app.get('/add?A=22&B=character')
 		self.assertEqual(b'22.0', solution.data)
 		#when one input is alphabet and other input be any number, whether rational , integer, fraction ultimately the result will be the input which was an integer
 
-	def test_add10(self):
+	def test_add9(self):
 
-		#case 10, when A input is of the form p/q where q=0 and B input be any number
+		#case 9, when A input is of the form p/q where q=0 and B input be any number
 		solution = self.app.get('add?A=1/0&B=98')
 		self.assertEqual(b"None", solution.data)
 		#according to the script if q=0 in p/q form then it should display an error but it is resolved using zerodivision module 
 
-	def test_add11(self):
+	def test_add10(self):
 
-		#case 11, when A input is any number and B=p/q form where q=0
+		#case 10, when A input is any number and B=p/q form where q=0
 		solution = self.app.get('add?A=10&B=2/0')
 		self.assertEqual(b"None", solution.data)
 
